af747c1a126f93a290a3715a14c59f59df152afe
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2022-02-19 16:18:50 +0800

    Removed firebase logic for now

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,25 @@ import 'package:wordle_clone/page/wordle_page.dart';
 import 'package:wordle_clone/widget/authenticator.dart';
 import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
+main() => runApp(const MyApp());
+
+// main() Firebase stuff
+// WidgetsFlutterBinding.ensureInitialized();
+// await Firebase.initializeApp(
+//   options: DefaultFirebaseOptions.currentPlatform,
+// );
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Authenticator(
-      child: NeumorphicApp(
-        title: 'Perthle',
-        theme: NeumorphicThemeData(
-          textTheme: const TextTheme().apply(fontFamily: 'Poppins'),
-        ),
-        home: const WordlePage(
-          word: 'PERTH',
-        ),
+    return NeumorphicApp(
+      title: 'Perthle',
+      theme: NeumorphicThemeData(
+        textTheme: const TextTheme().apply(fontFamily: 'Poppins'),
       ),
+      home: const WordlePage(word: 'PERTH'),
     );
   }
 }
