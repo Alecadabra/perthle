c56d84255a5c73be54812f3b71a692140e295d25
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2022-02-20 22:03:39 +0800

    Restyled share panel

--- lib/widget/share_panel.dart
+++ lib/widget/share_panel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:wordle_clone/controller/wordle_controller.dart';
 import 'package:wordle_clone/model/saved_game_state.dart';
@@ -21,41 +22,59 @@ class SharePanel extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Material(
-        // padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Expanded(
-              child: Text(
-                savedGameState.shareableString,
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
+          ),
+        ),
+        color: NeumorphicTheme.baseColor(context),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 10,
+                child: Text(savedGameState.shareableString),
               ),
-            ),
-            const Spacer(),
-            Row(
-              children: [
-                OutlinedButton(
-                  child: const Text('Share'),
-                  onPressed: () {
-                    Share.share(
-                      savedGameState.shareableString,
-                      subject: 'Perthle ${savedGameState.gameNum}',
-                    );
-                  },
-                ),
-                Tooltip(
-                  message: 'Copy to Clipboard',
-                  child: OutlinedButton(
-                    child: const Icon(Icons.copy_outlined),
-                    onPressed: () => Clipboard.setData(
-                      ClipboardData(
-                        text: savedGameState.shareableString,
+              const Spacer(),
+              Expanded(
+                flex: 2,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      flex: 13,
+                      child: OutlinedButton(
+                        child: const Text('Share'),
+                        onPressed: () {
+                          Share.share(
+                            savedGameState.shareableString,
+                            subject: 'Perthle ${savedGameState.gameNum}',
+                          );
+                        },
                       ),
                     ),
-                  ),
-                )
-              ],
-            )
-          ],
+                    const Spacer(),
+                    Expanded(
+                      flex: 5,
+                      child: Tooltip(
+                        message: 'Copy to Clipboard',
+                        child: OutlinedButton(
+                          child: const Icon(Icons.copy_outlined, size: 18),
+                          onPressed: () => Clipboard.setData(
+                            ClipboardData(text: savedGameState.shareableString),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
