64e20f17ebfe9ab5f81cbd9bcf641df3c3de823d
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2022-04-19 21:50:58 +0800

    Dissapearing history list

--- lib/page/history_page.dart
+++ lib/page/history_page.dart
@@ -1,57 +1,146 @@
+import 'dart:math';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:perthle/controller/history_cubit.dart';
 import 'package:perthle/model/history_state.dart';
+import 'package:perthle/model/saved_game_state.dart';
+import 'package:perthle/model/tile_match_state.dart';
 import 'package:perthle/widget/perthle_appbar.dart';
 import 'package:perthle/widget/perthle_scaffold.dart';
 
-class HistoryPage extends StatelessWidget {
+class HistoryPage extends StatefulWidget {
   const HistoryPage({final Key? key}) : super(key: key);
 
   static const LightSource lightSource = LightSource.topRight;
 
   @override
+  State<HistoryPage> createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  late ScrollController scroll;
+
+  double pixels = 0;
+  double maxScrollExtent = 0;
+  double viewportDimension = 0;
+
+  static const double _childHeight = _childPadding * 2 + _childInnerHeight;
+  static const double _childPadding = 10;
+  static const double _childInnerHeight = 100;
+
+  double _visibility(final int idx) {
+    double val = max(
+      0,
+      -((_childHeight * idx - pixels) / _childHeight - 1.5).abs() + 2,
+    );
+
+    return min(1, sqrt(val));
+  }
+
+  @override
+  void initState() {
+    scroll = ScrollController();
+    scroll.addListener(() {
+      setState(() {
+        pixels = scroll.position.pixels;
+        maxScrollExtent = scroll.position.maxScrollExtent;
+        viewportDimension = scroll.position.viewportDimension;
+      });
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(final BuildContext context) {
     return PerthleScaffold(
-      appBar: const PerthleAppbar(title: 'History', lightSource: lightSource),
+      appBar: const PerthleAppbar(
+          title: 'History', lightSource: HistoryPage.lightSource),
       body: Column(
         children: [
           const Spacer(),
           Expanded(
-            flex: 3,
+            flex: 4,
             child: BlocBuilder<HistoryCubit, HistoryState>(
-              builder: (final context, final history) {
+              builder: (final context, history) {
+                history = HistoryState(
+                    savedGames: List.generate(
+                        30,
+                        (final idx) =>
+                            SavedGameState(gameNum: idx, matches: const [
+                              [
+                                TileMatchState.match,
+                                TileMatchState.wrong,
+                                TileMatchState.wrong,
+                                TileMatchState.miss
+                              ],
+                              [
+                                TileMatchState.match,
+                                TileMatchState.wrong,
+                                TileMatchState.wrong,
+                                TileMatchState.miss
+                              ],
+                              [
+                                TileMatchState.match,
+                                TileMatchState.wrong,
+                                TileMatchState.wrong,
+                                TileMatchState.miss
+                              ],
+                              [
+                                TileMatchState.match,
+                                TileMatchState.wrong,
+                                TileMatchState.wrong,
+                                TileMatchState.miss
+                              ],
+                            ])).asMap());
                 return SizedBox(
                   width: 600,
-                  child: Neumorphic(
-                    style: const NeumorphicStyle(depth: -20),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 20),
-                      child: PhysicalModel(
-                        color: NeumorphicTheme.baseColor(context),
-                        elevation: 10,
-                        child: ListWheelScrollView(
-                          itemExtent: 150,
-                          diameterRatio: 1.8,
-                          squeeze: 0.9,
-                          scrollBehavior: const _HistoryScrollBehaviour(),
-                          children: [
-                            ...history.savedGames.values
-                                .expand((final element) =>
-                                    [element, element, element])
-                                .map(
-                              (final savedGame) {
-                                return Neumorphic(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(savedGame.shareableString(
-                                      'Perthle', true)),
-                                );
-                              },
-                            ).toList()
-                          ],
-                        ),
-                      ),
+                  child: ScrollConfiguration(
+                    behavior: const _HistoryScrollBehaviour(),
+                    child: ListView.builder(
+                      controller: scroll,
+                      padding: const EdgeInsets.all(30),
+                      addAutomaticKeepAlives: false,
+                      itemCount: history.savedGames.length,
+                      itemBuilder: (final context, final idx) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: _childPadding,
+                          ),
+                          child: SizedBox(
+                            height: _childInnerHeight,
+                            child: Neumorphic(
+                              style: NeumorphicStyle(
+                                depth: _visibility(idx) * 10,
+                              ),
+                              padding: const EdgeInsets.all(8),
+                              child: AnimatedOpacity(
+                                opacity: _visibility(idx),
+                                duration: Duration.zero,
+                                child: Text('${_visibility(idx)}'),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      // children: [
+                      //   ...history.savedGames.values
+                      //       .map(
+                      //         (final savedGame) {
+                      //           return Padding(
+                      //             padding: const EdgeInsets.only(bottom: 16),
+                      //             child: Neumorphic(
+                      //               padding: EdgeInsets.all(max(0, offset)),
+                      //               child: Text(savedGame.shareableString(
+                      //                   'Perthle', true)),
+                      //             ),
+                      //           );
+                      //         },
+                      //       )
+                      //       .toList()
+                      //       .reversed
+                      // ],
                     ),
                   ),
                 );
