37f92f4cf7034c49078129678e5ff16f6222e6ba
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2022-09-05 23:01:54 +0800

    Code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
