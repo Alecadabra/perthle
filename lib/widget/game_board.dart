7f172403f31137b6d0184f17f4733b190509e6b4
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2022-12-18 14:19:11 +0800

    Revealed tiles are smaller

--- lib/widget/game_board.dart
+++ lib/widget/game_board.dart
@@ -1,8 +1,10 @@
+import 'package:dartx/dartx.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:perthle/bloc/game_bloc.dart';
 import 'package:perthle/model/game_state.dart';
 import 'package:perthle/model/game_completion_state.dart';
+import 'package:perthle/model/tile_match_state.dart';
 import 'package:perthle/widget/board_tile.dart';
 
 /// The perthle board of guesses, made up of [BoardTile]s.
@@ -14,8 +16,11 @@ class GameBoard extends StatelessWidget {
     return RepaintBoundary(
       child: BlocBuilder<GameBloc, GameState>(
         builder: (final context, final outGame) {
+          final width = outGame.board.matches[0]
+              .filter((final match) => !match.isRevealed)
+              .length;
           return AspectRatio(
-            aspectRatio: outGame.board.width / outGame.board.height,
+            aspectRatio: width / outGame.board.height,
             child: Column(
               children: [
                 const Spacer(),
@@ -24,34 +29,49 @@ class GameBoard extends StatelessWidget {
                     flex: 2,
                     child: Row(
                       children: [
-                        const Spacer(),
                         for (var j = 0; j < outGame.board.width; j++) ...[
-                          Expanded(
-                            flex: 2,
-                            child: BlocBuilder<GameBloc, GameState>(
-                              builder: (final context, final inGame) {
-                                return BoardTile(
-                                  match: inGame.board.matches[i][j],
-                                  letter: inGame.board.letters[i][j],
-                                  lightSource: LightSource(
-                                    inGame.currCol == inGame.board.width ||
-                                            !inGame.completion.isPlaying
-                                        ? 0
-                                        : (inGame.currCol - j) /
-                                            inGame.board.width,
-                                    !inGame.completion.isPlaying
-                                        ? 0
-                                        : (inGame.currRow - i) /
-                                            inGame.board.height,
-                                  ),
-                                  current: inGame.completion.isPlaying &&
-                                          j == inGame.currCol &&
-                                          i == inGame.currRow ||
-                                      inGame.currCol == inGame.board.width &&
-                                          i == inGame.currRow,
-                                );
-                              },
-                            ),
+                          const Spacer(),
+                          BlocBuilder<GameBloc, GameState>(
+                            builder: (final context, final inGame) {
+                              return Expanded(
+                                flex: inGame.board.matches[i][j].isRevealed
+                                    ? 6
+                                    : 8,
+                                child: Row(
+                                  children: [
+                                    if (!inGame.board.matches[i][j].isRevealed)
+                                      const Spacer(),
+                                    Expanded(
+                                      flex: 6,
+                                      child: BoardTile(
+                                        match: inGame.board.matches[i][j],
+                                        letter: inGame.board.letters[i][j],
+                                        lightSource: LightSource(
+                                          inGame.currCol ==
+                                                      inGame.board.width ||
+                                                  !inGame.completion.isPlaying
+                                              ? 0
+                                              : (inGame.currCol - j) /
+                                                  inGame.board.width,
+                                          !inGame.completion.isPlaying
+                                              ? 0
+                                              : (inGame.currRow - i) /
+                                                  inGame.board.height,
+                                        ),
+                                        current: inGame.completion.isPlaying &&
+                                                j == inGame.currCol &&
+                                                i == inGame.currRow ||
+                                            inGame.currCol ==
+                                                    inGame.board.width &&
+                                                i == inGame.currRow,
+                                      ),
+                                    ),
+                                    if (!inGame.board.matches[i][j].isRevealed)
+                                      const Spacer(),
+                                  ],
+                                ),
+                              );
+                            },
                           ),
                           const Spacer(),
                         ],
