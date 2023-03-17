188470a15cf706237a56ba36c11449d17e530c69
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2023-03-17 15:45:42 +0800

    Revert test

--- lib/widget/game_board.dart
+++ lib/widget/game_board.dart
@@ -15,55 +15,63 @@ class GameBoard extends StatelessWidget {
   Widget build(final BuildContext context) {
     return RepaintBoundary(
       child: BlocBuilder<GameBloc, GameState>(
-        builder: (final context, final game) {
-          final width = game.board.matches[0]
+        builder: (final context, final outGame) {
+          final width = outGame.board.matches[0]
               .filter((final match) => !match.isRevealed)
               .length;
           return AspectRatio(
-            aspectRatio: width / game.board.height,
+            aspectRatio: width / outGame.board.height,
             child: Column(
               children: [
                 const Spacer(),
-                for (var i = 0; i < game.board.height; i++) ...[
+                for (var i = 0; i < outGame.board.height; i++) ...[
                   Expanded(
                     flex: 2,
                     child: Row(
                       children: [
-                        for (var j = 0; j < game.board.width; j++) ...[
+                        for (var j = 0; j < outGame.board.width; j++) ...[
                           const Spacer(),
-                          Expanded(
-                            flex: game.board.matches[i][j].isRevealed ? 6 : 8,
-                            child: Row(
-                              children: [
-                                if (!game.board.matches[i][j].isRevealed)
-                                  const Spacer(),
-                                Expanded(
-                                  flex: 6,
-                                  child: BoardTile(
-                                    match: game.board.matches[i][j],
-                                    letter: game.board.letters[i][j],
-                                    lightSource: LightSource(
-                                      game.currCol == game.board.width ||
-                                              !game.completion.isPlaying
-                                          ? 0
-                                          : (game.currCol - j) /
-                                              game.board.width,
-                                      !game.completion.isPlaying
-                                          ? 0
-                                          : (game.currRow - i) /
-                                              game.board.height,
+                          BlocBuilder<GameBloc, GameState>(
+                            builder: (final context, final inGame) {
+                              return Expanded(
+                                flex: inGame.board.matches[i][j].isRevealed
+                                    ? 6
+                                    : 8,
+                                child: Row(
+                                  children: [
+                                    if (!inGame.board.matches[i][j].isRevealed)
+                                      const Spacer(),
+                                    Expanded(
+                                      flex: 6,
+                                      child: BoardTile(
+                                        match: inGame.board.matches[i][j],
+                                        letter: inGame.board.letters[i][j],
+                                        lightSource: LightSource(
+                                          inGame.currCol ==
+                                                      inGame.board.width ||
+                                                  !inGame.completion.isPlaying
+                                              ? 0
+                                              : (inGame.currCol - j) /
+                                                  inGame.board.width,
+                                          !inGame.completion.isPlaying
+                                              ? 0
+                                              : (inGame.currRow - i) /
+                                                  inGame.board.height,
+                                        ),
+                                        current: inGame.completion.isPlaying &&
+                                                j == inGame.currCol &&
+                                                i == inGame.currRow ||
+                                            inGame.currCol ==
+                                                    inGame.board.width &&
+                                                i == inGame.currRow,
+                                      ),
                                     ),
-                                    current: game.completion.isPlaying &&
-                                            j == game.currCol &&
-                                            i == game.currRow ||
-                                        game.currCol == game.board.width &&
-                                            i == game.currRow,
-                                  ),
+                                    if (!inGame.board.matches[i][j].isRevealed)
+                                      const Spacer(),
+                                  ],
                                 ),
-                                if (!game.board.matches[i][j].isRevealed)
-                                  const Spacer(),
-                              ],
-                            ),
+                              );
+                            },
                           ),
                           const Spacer(),
                         ],
@@ -75,6 +83,9 @@ class GameBoard extends StatelessWidget {
               ],
             ),
           );
+        },
+        buildWhen: (final a, final b) {
+          return a.gameNum != b.gameNum;
         },
       ),
     );
