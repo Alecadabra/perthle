09112499d7bc66b8fb0262232b31266eb2f4943b
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2023-09-19 21:32:52 +0800

    System theming

--- lib/widget/init_loader.dart
+++ lib/widget/init_loader.dart
@@ -4,79 +4,86 @@ import 'package:perthle/bloc/init_cubit.dart';
 import 'package:perthle/model/character_state.dart';
 import 'package:perthle/model/init_state.dart';
 import 'package:perthle/widget/board_tile.dart';
-import 'package:perthle/widget/perthle_provider.dart';
+import 'package:perthle/widget/perthle_scaffold.dart';
+import 'package:shimmer/shimmer.dart';
 
 class InitLoader extends StatelessWidget {
-  const InitLoader({super.key, required this.child});
-
-  final Widget child;
+  const InitLoader({super.key});
 
   @override
   Widget build(final BuildContext context) {
-    return BlocBuilder<InitCubit, InitState>(
-      builder: (final context, final initState) {
-        final dailyState = initState.initialDaily;
-        return AnimatedSwitcher(
-          duration: const Duration(seconds: 2),
-          child: dailyState != null
-              ? PerthleProvider(
-                  initialDaily: dailyState,
-                  child: child,
-                )
-              : _InitLoadStage(loadStage: initState.loadStage),
-        );
-      },
+    return PerthleScaffold(
+      appBar: NeumorphicAppBar(
+        centerTitle: true,
+        title: Shimmer.fromColors(
+          baseColor: NeumorphicTheme.defaultTextColor(context),
+          highlightColor: NeumorphicTheme.disabledColor(context),
+          child: Container(
+            decoration: BoxDecoration(
+              color: NeumorphicTheme.baseColor(context),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            height: 40,
+            width: 180,
+          ),
+        ),
+      ),
+      body: const _InitLoadStage(),
     );
   }
 }
 
 class _InitLoadStage extends StatelessWidget {
-  const _InitLoadStage({required this.loadStage});
-
-  final InitStateEnum loadStage;
+  const _InitLoadStage();
 
   @override
   Widget build(final BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          ' ',
-          style: Theme.of(context).textTheme.bodyMedium,
-        ),
-        SizedBox(
-          height: 80,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              for (int i = 0; i < 7; i++)
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 16,
-                    bottom: 16,
-                    right: 6,
-                    left: 6,
-                  ),
-                  child: BoardTile(
-                    match: loadStage.matches[i],
-                    scale: 5,
-                    letter: CharacterState(loadStage.word[i]),
-                    lightSource: LightSource(
-                      1 - loadStage.index / InitStateEnum.values.length * 2,
-                      1 - loadStage.index / InitStateEnum.values.length,
+    return BlocBuilder<InitCubit, InitState>(
+      buildWhen: (final a, final b) => a.loadStage.word != b.loadStage.word,
+      builder: (final context, final initState) {
+        final loadStage = initState.loadStage;
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              ' ',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            SizedBox(
+              height: 80,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  for (int i = 0; i < 7; i++)
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16,
+                        bottom: 16,
+                        right: 6,
+                        left: 6,
+                      ),
+                      child: BoardTile(
+                        match: loadStage.matches[i],
+                        scale: 5,
+                        letter: CharacterState(loadStage.word[i]),
+                        lightSource: LightSource(
+                          1 - loadStage.index / InitStateEnum.values.length * 2,
+                          1 - loadStage.index / InitStateEnum.values.length,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-            ],
-          ),
-        ),
-        Text(
-          loadStage.message,
-          style: Theme.of(context).textTheme.bodyMedium,
-        ),
-      ],
+                ],
+              ),
+            ),
+            Text(
+              loadStage.message,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        );
+      },
     );
   }
 }
