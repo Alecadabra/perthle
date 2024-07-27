55dbcadfbc240b4aa93eae1b90cfcfa526864eff
Alecadabra <27219575+Alecadabra@users.noreply.github.com>
2024-07-27 17:51:26 +0800

    Switch to downloading temp export

--- lib/widget/perthle_appbar.dart
+++ lib/widget/perthle_appbar.dart
@@ -1,13 +1,17 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:perthle/bloc/history_cubit.dart';
 import 'package:perthle/bloc/messenger_cubit.dart';
 import 'package:perthle/model/history_state.dart';
 import 'package:perthle/model/saved_game_state.dart';
-import 'package:share_plus/share_plus.dart';
+import 'dart:html' as web_file;
 
 /// The appbar that shows a title string for use within a perthle scaffold.
 class PerthleAppbar extends StatelessWidget {
@@ -19,6 +23,16 @@ class PerthleAppbar extends StatelessWidget {
 
   final String title;
   final LightSource lightSource;
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/saved_games.json');
+  }
 
   @override
   Widget build(final BuildContext context) {
@@ -107,24 +121,53 @@ class PerthleAppbar extends StatelessWidget {
                 TextButton(
                   child: const Text('EXPORT'),
                   onPressed: () async {
+                    final messenger = MessengerCubit.of(context);
                     final history = HistoryCubit.of(context);
                     final json = history.toJson(history.state);
 
+                    messenger.sendMessage('Stringifying');
                     final stringified = const JsonEncoder().convert(json);
 
-                    await Clipboard.setData(ClipboardData(text: stringified));
+                    if (kIsWeb) {
+                      messenger.sendMessage('Encoding');
+                      final bytes = utf8.encode(stringified);
+                      messenger.sendMessage('Blobbing');
+                      final blob = web_file.Blob(
+                        [bytes],
+                        'text/plain',
+                        'native',
+                      );
 
-                    showDialog(
-                      context: context,
-                      builder: (final BuildContext context) {
-                        return Dialog(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: SelectableText(stringified),
-                          ),
-                        );
-                      },
-                    );
+                      messenger.sendMessage('Downloading 1');
+                      final href = web_file.Url.createObjectUrlFromBlob(
+                        blob,
+                      ).toString();
+
+                      messenger.sendMessage('Downloading 2');
+                      var anchorElement = web_file.AnchorElement(href: href);
+
+                      messenger.sendMessage('Downloading 3');
+                      anchorElement.setAttribute(
+                        'download',
+                        'saved_games.json',
+                      );
+
+                      messenger.sendMessage('Downloading 4');
+                      anchorElement.click();
+                    }
+                    // await Clipboard.setData(ClipboardData(text: stringified));
+
+                    // showDialog(
+                    //   context: context,
+                    //   builder: (final BuildContext context) {
+                    //     return Dialog(
+                    //       child: Padding(
+                    //         padding: const EdgeInsets.all(8.0),
+                    //         child: SelectableText(stringified),
+                    //       ),
+                    //     );
+                    //   },
+                    // );
 
                     ScaffoldMessenger.of(context).clearMaterialBanners();
                   },
